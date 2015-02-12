b86386c7e19f4af354e8fbcf54cc62b762b007d1
Tony Lukasavage <anthony.lukasavage@gmail.com>
2015-02-12 18:36:55 -0500

    allow for question bundles

--- appc-inquirer.js
+++ appc-inquirer.js
@@ -26,17 +26,19 @@ AppcInquirer.prototype.prompt = function prompt(questions, opts, callback) {
 function SocketPrompt(opts) {
 	this.host = opts.host || '127.0.0.1';
 	this.port = opts.port || 22212;
+	this.bundle = opts.bundle || false;
 }
 
 SocketPrompt.prototype.prompt = function(questions, callback) {
 	questions = !Array.isArray(questions) ? [questions] : questions;
 
+	var self = this;
 	var client = net.connect({ 
 		host: this.host,
 		port: this.port 
-	}), answers = {};
+	});
 
-	async.series([
+	async.waterfall([
 
 		// set up handlers for socket
 		function(cb) {
@@ -46,67 +48,193 @@ SocketPrompt.prototype.prompt = function(questions, callback) {
 
 		// send question, receive answer
 		function(cb) {
-			async.eachSeries(questions, function(q, done) {
-				// when
-				if (isFunction(q.when) && !q.when(answers)) { return done(); }
-
-				// message, default, and choices can be a function
-				if (isFunction(q.message)) { q.message = q.message(answers); }
-				if (isFunction(q.default)) { q.default = q.default(answers); }
-				if (isFunction(q.choices)) { q.choices = q.choices(answers); }
-
-				// send question over socket
-				client.write(JSON.stringify({
-					type: 'question',
-					question: q
-				}));
-
-				function waitForResponse() {
-					client.once('data', function(answer) {
-						// make sure we got JSON back
-						try {
-							answer = JSON.parse(answer);	
-						} catch (e) {
-							client.write(JSON.stringify({
-								type: 'error',
-								message: 'parse error: ' + e.message,
-								question: q
-							}));
-							return waitForResponse();
-						}
-
-						// validate the answer
-						if (isFunction(q.validate)) {
-							var valid = q.validate(answer);
-							if (valid !== true) {
-								client.write(JSON.stringify({
-									type: 'error',
-									message: 'validate error: ' + (valid || 'invalid value for ' + q.name ),
-									question: q
-								}));
-								return waitForResponse();
-							}
-						}
-
-						// filter
-						if (isFunction(q.filter)) {
-							answer = q.filter(answer);
-						}
-
-						// save answer
-						answers[q.name] = answer;
-						return done();
-					});
-				}
-
-				return waitForResponse();
-			}, cb);
+			if (self.bundle) {
+				return bundleQuestions(client, questions, cb);
+			} else {
+				return singleQuestions(client, questions, cb);
+			}
 		}
-	], function(err) {
+	], function(err, answers) {
 		client.end();
 		return callback(err, answers);
 	});
 };
+
+function singleQuestions(client, questions, callback) {
+	var answers = {};
+
+	async.eachSeries(questions, function(q, done) {
+		// when
+		if (isFunction(q.when) && !q.when(answers)) { return done(); }
+
+		// message, default, and choices can be a function
+		if (isFunction(q.message)) { q.message = q.message(answers); }
+		if (isFunction(q.default)) { q.default = q.default(answers); }
+		if (isFunction(q.choices)) { q.choices = q.choices(answers); }
+
+		// send question over socket
+		client.write(JSON.stringify({
+			type: 'question',
+			question: q
+		}));
+
+		function waitForResponse() {
+			client.once('data', function(answer) {
+				// make sure we got JSON back
+				try {
+					answer = JSON.parse(answer);	
+				} catch (e) {
+					client.write(JSON.stringify({
+						type: 'error',
+						message: 'parse error: ' + e.message,
+						question: q
+					}));
+					return waitForResponse();
+				}
+
+				// validate the answer
+				if (isFunction(q.validate)) {
+					var valid = q.validate(answer);
+					if (valid !== true) {
+						client.write(JSON.stringify({
+							type: 'error',
+							message: 'validate error: ' + (valid || 'invalid value for ' + q.name ),
+							question: q
+						}));
+						return waitForResponse();
+					}
+				}
+
+				// filter
+				if (isFunction(q.filter)) {
+					answer = q.filter(answer);
+				}
+
+				// save answer
+				answers[q.name] = answer;
+				return done();
+			});
+		}
+
+		return waitForResponse();
+	}, function(err) {
+		return callback(err, answers);
+	});
+}
+
+function bundleQuestions(client, questions, callback) {
+	var answers = {},
+		bundles = [];
+	
+	// create question bundles
+	questions.forEach(function(q, index) {
+		if (isFunction(q.when) || isFunction(q.message) || isFunction(q.default) || 
+			isFunction(q.choices) || index === 0) 
+		{
+			bundles[bundles.length] = [q];
+		} else {
+			bundles[bundles.length-1].push(q);
+		}
+	});
+
+	// process each question bundle over socket
+	async.eachSeries(bundles, function(bundle, done) {
+		var reqBundle = [];
+		bundle.forEach(function(q) {
+			// when
+			if (isFunction(q.when) && !q.when(answers)) { return; }
+
+			// message, default, and choices can be a function
+			if (isFunction(q.message)) { q.message = q.message(answers); }
+			if (isFunction(q.default)) { q.default = q.default(answers); }
+			if (isFunction(q.choices)) { q.choices = q.choices(answers); }
+
+			reqBundle.push(q);
+		});
+
+		// nothing to ask
+		if (reqBundle.length === 0) { return done(); }
+
+		// send question over socket
+		client.write(JSON.stringify({
+			type: 'question',
+			question: reqBundle
+		}));
+
+		function waitForResponse() {
+			client.once('data', function(respAnswers) {
+				// make sure we got JSON back
+				try {
+					respAnswers = JSON.parse(respAnswers);	
+				} catch (e) {
+					client.write(JSON.stringify({
+						type: 'error',
+						message: 'parse error: ' + e.message,
+						question: reqBundle
+					}));
+					return waitForResponse();
+				}
+
+				// validate the answer
+				var keys = Object.keys(respAnswers);
+				for (var i = 0; i < keys.length; i++) {
+					var key = keys[i];
+					var q = find(reqBundle, 'name', key);
+					var answer = respAnswers[key];
+
+					// validate the current answer
+					if (isFunction(q.validate)) {
+						var valid = q.validate(answer);
+						if (valid !== true) {
+							client.write(JSON.stringify({
+								type: 'error',
+								message: 'validate error: ' + (valid || 'invalid value for ' + q.name ),
+								question: reqBundle
+							}));
+							return waitForResponse();
+						}
+					}
+
+					// filter the answer 
+					if (isFunction(q.filter)) {
+						answer = q.filter(answer);
+					}
+
+					// save the answer
+					answers[key] = answer;
+				}
+
+				return done();
+			});
+		}
+
+		return waitForResponse();
+	}, function(err) {
+		return callback(err, answers);
+	});
+}
+
+function processResponse(resp) {
+	// make sure we got JSON back
+	try {
+		answer = JSON.parse(answer);	
+	} catch (e) {
+		client.write(JSON.stringify({
+			type: 'error',
+			message: 'parse error: ' + e.message,
+			question: q
+		}));
+		return waitForResponse();
+	}
+}
+
+function find(array, key, value) {
+	for (var i = 0; i < array.length; i++) {
+		var item = array[i];
+		if (item[key] === value) { return item; }
+	}
+	return null;
+}
 
 function isFunction(o) {
 	return o && Object.prototype.toString.call(o) === '[object Function]';
--- test/appc-inquirer_test.js
+++ test/appc-inquirer_test.js
@@ -91,6 +91,36 @@ describe('appc-inquirer', function() {
 		});
 	});
 
+	it('gets user input via socket-bundle', function(done) {
+		var val = 'workit';
+
+		var server = net.createServer(function(c) {
+			c.once('data', function(data) {
+				server.close();
+				(function() {
+					data = JSON.parse(data.toString());
+				}).should.not.throw();
+				data.should.be.an.Object;
+				data.type.should.equal('question');
+				data.question.should.be.an.Array;
+				data.question.should.have.length(1);
+				data.question[0].should.have.properties(BASIC);
+
+				var result = {};
+				result[data.question[0].name] = val;
+				c.write(JSON.stringify(result));
+			});
+		});
+		server.listen(DEFAULT_PORT, function() {
+			inquirer.prompt([BASIC], {socket:true,bundle:true}, function(err, answers) {
+				should.not.exist(err);
+				answers.should.be.an.Object;
+				answers.basic.should.equal(val);
+				return done();
+			});
+		});
+	});
+
 	it('gets user input via socket with single question', function(done) {
 		var val = 'workit';
 
@@ -141,6 +171,33 @@ describe('appc-inquirer', function() {
 		});
 	});
 
+	it('gets user input via socket-bundle with complex question', function(done) {
+		var server = net.createServer(function(c) {
+			c.once('data', function(data) {
+				server.close();
+				(function() {
+					data = JSON.parse(data.toString());
+				}).should.not.throw();
+				data.should.be.an.Object;
+				data.type.should.equal('question');
+				data.question.should.be.an.Array;
+				data.question[0].should.have.properties(_.omit(COMPLEX, OMIT_PROPS));
+
+				var result = {};
+				result[data.question[0].name] = 'defaultcomplex';
+				c.write(JSON.stringify(result));
+			});
+		});
+		server.listen(DEFAULT_PORT, function() {
+			inquirer.prompt([COMPLEX], {socket:true,bundle:true}, function(err, answers) {
+				should.not.exist(err);
+				answers.should.be.an.Object;
+				answers.complex.should.equal('DEFAULTCOMPLEX');
+				return done();
+			});
+		});
+	});
+
 	it('skips questions that fail when() checks', function(done) {
 		var question = _.clone(BASIC);
 		question.when = function(answers) {
@@ -152,6 +209,25 @@ describe('appc-inquirer', function() {
 		});
 		server.listen(DEFAULT_PORT, function() {
 			inquirer.prompt(question, {socket:true}, function(err, answers) {
+				should.not.exist(err);
+				answers.should.be.an.Object;
+				answers.should.be.empty;
+				return done();
+			});
+		});
+	});
+
+	it('skips questions that fail when() checks (socket-bundle)', function(done) {
+		var question = _.clone(BASIC);
+		question.when = function(answers) {
+			return false;
+		};
+
+		var server = net.createServer(function(c) {
+			server.close();
+		});
+		server.listen(DEFAULT_PORT, function() {
+			inquirer.prompt(question, {socket:true,bundle:true}, function(err, answers) {
 				should.not.exist(err);
 				answers.should.be.an.Object;
 				answers.should.be.empty;
@@ -184,6 +260,40 @@ describe('appc-inquirer', function() {
 		});
 		server.listen(DEFAULT_PORT, function() {
 			inquirer.prompt([LIST], {socket:true}, function(err, answers) {
+				should.not.exist(err);
+				answers.should.be.an.Object;
+				answers.list.should.equal('answer2_value');
+				return done();
+			});
+		});
+	});
+
+	it('gets user input via socket-bundle with complex lists', function(done) {
+		var server = net.createServer(function(c) {
+			c.once('data', function(data) {
+				server.close();
+				(function() {
+					data = JSON.parse(data.toString());
+				}).should.not.throw();
+				data.should.be.an.Object;
+				data.type.should.equal('question');
+				data.question.should.be.an.Array;
+				data.question[0].should.have.properties(_.omit(LIST, OMIT_PROPS));
+
+				for (var i = 0; i < data.question[0].choices.length; i++) {
+					var choice = data.question[0].choices[i];
+					choice.should.be.an.Object;
+					choice.name.should.equal('answer' + (i+1) + '_name');
+					choice.value.should.equal('answer' + (i+1) + '_value');
+				}
+
+				var result = {};
+				result[data.question[0].name] = 'answer2_value';
+				c.write(JSON.stringify(result));
+			});
+		});
+		server.listen(DEFAULT_PORT, function() {
+			inquirer.prompt([LIST], {socket:true,bundle:true}, function(err, answers) {
 				should.not.exist(err);
 				answers.should.be.an.Object;
 				answers.list.should.equal('answer2_value');
@@ -237,6 +347,53 @@ describe('appc-inquirer', function() {
 		});
 	});
 
+	it('resends question when it receives bad response JSON (socket-bundle)', function(done) {
+		var val = 'workit';
+
+		var server = net.createServer(function(c) {
+
+			// handle initial question
+			c.once('data', function(data) {
+				(function() {
+					data = JSON.parse(data.toString());
+				}).should.not.throw();
+				data.should.be.an.Object;
+				data.type.should.equal('question');
+				data.question.should.be.an.Array;
+				data.question[0].should.have.properties(BASIC);
+
+				// send bad JSON as response
+				c.write(JSON.stringify(val) + '}}');
+
+				// handle error response, try again
+				c.once('data', function(data) {
+					server.close();
+					(function() {
+						data = JSON.parse(data.toString());
+					}).should.not.throw();
+					data.should.be.an.Object;
+					data.type.should.equal('error');
+					data.message.should.match(/parse error/);
+					data.question.should.be.an.Array;
+					data.question[0].should.have.properties(BASIC);
+
+					// send good JSON this time
+					var result = {};
+					result[data.question[0].name] = val;
+					c.write(JSON.stringify(result));
+				});
+			});
+		});
+		server.listen(DEFAULT_PORT, function() {
+			inquirer.prompt(BASIC, {socket:true,bundle:true}, function(err, answers) {
+				should.not.exist(err);
+				answers.should.be.an.Object;
+				answers.basic.should.equal(val);
+				return done();
+			});
+		});
+	});
+
 	it('resends question when answer fails validation (default message)', function(done) {
 		var val = 'workit', 
 			question = _.clone(BASIC);
@@ -278,6 +435,59 @@ describe('appc-inquirer', function() {
 		});
 		server.listen(DEFAULT_PORT, function() {
 			inquirer.prompt(question, {socket:true}, function(err, answers) {
+				should.not.exist(err);
+				answers.should.be.an.Object;
+				answers.basic.should.equal(val);
+				return done();
+			});
+		});
+	});
+
+	it('resends question when answer fails validation socket-bundle (default message)', function(done) {
+		var val = 'workit', 
+			question = _.clone(BASIC);
+		question.validate = function(answer) {
+			return answer === val;
+		};
+
+		var server = net.createServer(function(c) {
+
+			// handle initial question
+			c.once('data', function(data) {
+				(function() {
+					data = JSON.parse(data.toString());
+				}).should.not.throw();
+				data.should.be.an.Object;
+				data.type.should.equal('question');
+				data.question.should.be.an.Array;
+				data.question[0].should.have.properties(_.omit(question, OMIT_PROPS));
+
+				// send bad JSON as response
+				var result = {};
+				result[data.question[0].name] = val + '}}';
+				c.write(JSON.stringify(result));
+
+				// handle error response, try again
+				c.once('data', function(data) {
+					server.close();
+					(function() {
+						data = JSON.parse(data.toString());
+					}).should.not.throw();
+					data.should.be.an.Object;
+					data.type.should.equal('error');
+					data.message.should.match(/validate error/);
+					data.question.should.be.an.Array;
+					data.question[0].should.have.properties(_.omit(question, OMIT_PROPS));
+
+					// send good JSON this time
+					result = {};
+					result[data.question[0].name] = val;
+					c.write(JSON.stringify(result));
+				});
+			});
+		});
+		server.listen(DEFAULT_PORT, function() {
+			inquirer.prompt(question, {socket:true,bundle:true}, function(err, answers) {
 				should.not.exist(err);
 				answers.should.be.an.Object;
 				answers.basic.should.equal(val);
@@ -375,6 +585,115 @@ describe('appc-inquirer', function() {
 
 		server.listen(9374, function() {
 			inquirer.prompt(questions, {socket:true, port:9374}, function(err, answers) {
+				should.not.exist(err);
+				answers.should.be.an.Object;
+				answers.q1.should.equal('answer#1');
+				answers.q2.should.equal('answer2');
+				answers.q3.should.eql(['v1','v3']);
+				answers.q4.should.equal(val);
+				answers.q5.should.be.true;
+				server.close();
+				return done();
+			});
+		});
+	});
+
+	it('handles many questions, with validation failures (socket-bundle)', function(done) {
+		var val = 'workit';
+		var questions = [
+			{
+				type: 'input',
+				name: 'q1'
+			},
+			{
+				type: 'list',
+				name: 'q2',
+				message: function(answers) {
+					return 'q2 for ' + answers.q1;
+				},
+				default: '3',
+				choices: ['1','2','3','4'],
+				filter: function(answer) {
+					return 'answer' + answer;
+				}
+			},
+			{
+				type: 'checkbox',
+				name: 'q3',
+				choices: [
+					{ name: 'a1', value: 'v1', checked: true },
+					{ name: 'a2', value: 'v2', checked: false },
+					{ name: 'a3', value: 'v3', checked: true },
+					{ name: 'a4', value: 'v4', checked: false }
+				],
+				when: function(answers) {
+					return !!answers.q2;
+				}
+			},
+			{
+				type: 'input',
+				name: 'q4',
+				validate: function(answer) {
+					return answer === val ? true : 'failed!!!';
+				}
+			},
+			{
+				type: 'confirm',
+				name: 'q5',
+				default: true
+			}
+		];
+
+		var server = net.createServer(function(c) {
+			c.on('data', function(data) {
+				(function() { data = JSON.parse(data.toString()); }).should.not.throw();
+				data.should.be.an.Object;
+				if (data.type === 'error') { return; }
+
+				switch (data.question[0].name) {
+					case 'q1':
+						data.type.should.equal('question');
+						data.question[0].should.have.properties(_.omit(questions[0], OMIT_PROPS));
+						c.write(JSON.stringify({ q1: 'answer#1' }));
+						break;
+					case 'q2':
+						data.type.should.equal('question');
+						data.question[0].should.have.properties(_.omit(questions[1], OMIT_PROPS));
+						c.write(JSON.stringify({ q2: '2' }));
+						break;
+					case 'q3':
+						data.type.should.equal('question');
+						data.question[0].should.have.properties(_.omit(questions[2], OMIT_PROPS));
+						data.question[1].should.have.properties(_.omit(questions[3], OMIT_PROPS));
+						data.question[2].should.have.properties(_.omit(questions[4], OMIT_PROPS));
+						c.write(JSON.stringify({
+							q3: ['v1','v3'],
+							q4: val + '!!!!',
+							q5: true
+						}));
+
+						c.once('data', function(data) {
+							(function() { data = JSON.parse(data.toString()); }).should.not.throw();
+							data.type.should.equal('error');
+							data.message.should.match(/failed!!!/);
+							data.question[0].should.have.properties(_.omit(questions[2], OMIT_PROPS));
+							data.question[1].should.have.properties(_.omit(questions[3], OMIT_PROPS));
+							data.question[2].should.have.properties(_.omit(questions[4], OMIT_PROPS));
+							c.write(JSON.stringify({
+								q3: ['v1','v3'],
+								q4: val,
+								q5: true
+							}));
+						});
+						break;
+					default:
+						throw new Error('should not get here');
+				}
+			});
+		});
+
+		server.listen(9374, function() {
+			inquirer.prompt(questions, {socket:true, port:9374, bundle:true}, function(err, answers) {
 				should.not.exist(err);
 				answers.should.be.an.Object;
 				answers.q1.should.equal('answer#1');
