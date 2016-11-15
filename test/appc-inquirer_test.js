d27cfa319da2d9b1eea18e7d163e45dc6562b9f1
Jan Vennemann <jvennemann@appcelerator.com>
2016-11-15 19:11:03 +0100

    [CLI-1133] Skip failing test during Jenkins build

--- test/appc-inquirer_test.js
+++ test/appc-inquirer_test.js
@@ -53,7 +53,7 @@ describe('appc-inquirer', function () {
 		inquirer.prompt.should.be.a.Function;
 	});
 
-	it('gets user input via command line', function (done) {
+	(process.env.JENKINS_URL ? it.skip : it)('gets user input via command line', function (done) {
 		var val = 'workit';
 		inquirer.prompt([BASIC], function (err, answers) {
 			should.not.exist(err);
